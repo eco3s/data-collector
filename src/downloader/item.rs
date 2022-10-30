387d52def59d35b2d17f7b57eb9aceb8a5a3359c
Abiria <abiria.dev@gmail.com>
2022-10-31 01:36:38 +0900

    feat: add parallel download function

--- src/downloader/item.rs
+++ src/downloader/item.rs
@@ -1,23 +1,60 @@
-use std::error::Error;
+use rayon::prelude::{IntoParallelRefIterator, ParallelIterator};
 
 use super::list::N;
 
 const BASEURL: &str = "https://kias.nie.re.kr/home/for/for02002v.do";
 
-pub fn get_items(list: Vec<N>) -> Result<Vec<String>, Box<dyn Error>> {
-	let mut res = vec![];
+trait FetchBulk {
+	type Output: Send + Sync;
+	type Url: Send + Sync;
 
-	for item in list {
-		res.push(
-			ureq::post(BASEURL)
-				.set("Accept", "application/json")
-				.send_form(&[
-					("clsSno", &item.to_string()),
-					("searchClsGbn", "eco"),
-				])?
-				.into_string()?,
-		);
+	fn get_urls(&self) -> &Vec<Self::Url>;
+	fn new(urls: Vec<Self::Url>) -> Self;
+	fn fetch(url: &Self::Url) -> Self::Output;
+	fn fetch_all(&self) -> Vec<Self::Output> {
+		self.get_urls()
+			.par_iter()
+			.map(Self::fetch)
+			.collect::<Vec<Self::Output>>()
 	}
+}
 
-	Ok(res)
+struct FetchBulkImpl {
+	urls: Vec<N>,
+}
+
+impl FetchBulk for FetchBulkImpl {
+	type Output = Result<String, ureq::Error>;
+	type Url = N;
+
+	fn get_urls(&self) -> &Vec<Self::Url> { &self.urls }
+
+	fn new(urls: Vec<Self::Url>) -> Self { Self { urls } }
+
+	fn fetch(url: &Self::Url) -> Self::Output {
+		Ok(ureq::post(BASEURL)
+			.set("Accept", "application/json")
+			.send_form(&[
+				("clsSno", &url.to_string()),
+				("searchClsGbn", "eco"),
+			])?
+			.into_string()?)
+	}
+}
+
+pub fn get_items(list: Vec<N>) -> Result<Vec<String>, Box<ureq::Error>> {
+	list.iter()
+		.map(FetchBulkImpl::fetch)
+		.map(|v| v.map_err(Box::new))
+		.collect::<Result<Vec<_>, _>>()
+}
+
+pub fn get_items_parallel(
+	list: Vec<N>,
+) -> Result<Vec<String>, Box<ureq::Error>> {
+	FetchBulkImpl::new(list)
+		.fetch_all()
+		.into_iter()
+		.map(|v| v.map_err(Box::new))
+		.collect::<Result<Vec<_>, _>>()
 }
